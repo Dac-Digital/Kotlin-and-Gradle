cfc0a156639114485fe8fc56f0b043c1109a5b80
Dac-Digital <144015159+Dac-Digital@users.noreply.github.com>
2023-09-05 13:58:23 +0200

    Create 1.kt

---
+++ 1.kt
@@ -0,0 +1,9 @@
+// excerpt from Versions.kt
+
+
+object Versions {
+   val kotest = "5.5.5"
+   val kotestKtorAssertions = "1.0.3"
+   val kotestPropertyArbs = "2.1.2"
+   val kotestTestContainers = "1.3.4"
+}
