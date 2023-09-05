0b8dd11bf9df950436b9fe43541d2e92c17c45bb
Dac-Digital <144015159+Dac-Digital@users.noreply.github.com>
2023-09-05 13:59:11 +0200

    Create 5.kt

---
+++ 5.kt
@@ -0,0 +1,11 @@
+# excerpt from gradle.properties
+kotlin.code.style=official
+
+
+
+
+# jib
+acr=petprojectcontainerregisterprodwe.azurecr.io
+imageName=pet-project
+baseImageName=gcr.io/distroless/java:11-debug
+dockerPort=8080
